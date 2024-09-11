fcaf687b92c00bca9bc663ff3583a356bd38227d
MadhelKathlynAnnJavillonar <qmkaajavillonar@tip.edu.ph>
2024-09-11 13:01:42 +0800

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+	// Initialization of T.I.P. Grading System
+	int x;
+   
+	cout<<"input grades:"<<endl;
+	cin>>x;
+	
+	//Process , of computing the grades
+	 
+	if (x>94)
+	{
+		cout<<"Execellent"<<endl;
+	}
+	else (x<94)
+	{
+		cout<<"Superior"<<endl;
+	}
+	else (x)
+	 {
+		cout<<"x is equal to y"<<endl;
+	}
+	
+	
+	
+	
+  return 0;
+}
+
