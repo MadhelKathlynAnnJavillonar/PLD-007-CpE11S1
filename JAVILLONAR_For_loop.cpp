7feebf147fd523d3fcc0cc1cdec21ba182b1f0f0
MadhelKathlynAnnJavillonar <qmkaajavillonar@tip.edu.ph>
2024-09-16 12:27:55 +0800

    Exercise #4 (For, Do, Do while & Let's Try)

---
+++ JAVILLONAR_For_loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+	main()
+	{
+		int i;
+		int j =10;
+		int k =10;
+		
+		for(i=0;i<=j;i++)
+	{
+	
+	cout<<"Hello"<<i << endl;
+		
+	}
+		
+		
+		
+		return 0;
+	}
+	
+	
+	
---
+++ Let's_Try_JAVILLONAR.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+
+	//C++ that displays integers less than 10 but not less than 3
+
+{
+	int i=3;
+	
+	do
+	{
+	cout<<"Value of integers less that 10 but not less than 3 is "<<i<<endl;
+	i++;
+	}
+	while(i<=9&& i>=0);
+	
+	return 0;
+}
+
